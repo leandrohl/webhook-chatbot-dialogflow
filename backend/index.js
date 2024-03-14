1bb4df527b1d9c0610a675588df5b71d780a5f87
“Leandro <leandro.silva.ext@vr.com.br>
2024-03-14 10:28:28 -0300

    feat: adicionando rotas do dialogflow

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import rotaDoce from './Rotas/rotaDoce.js'
+import rotaDialogFlow from './Rotas/rotaDialogFlow.js'
 
 const porta = 3000;
-const host = '0.0.0.0';
 
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
 app.use('/doces', rotaDoce)
+app.use('/dialogflow', rotaDialogFlow)
 
 app.listen(3000, () => {
     console.log(`Servidor rodando em http://localhost:3000`);
